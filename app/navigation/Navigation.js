ce0336d0273fe976221a9b39cc443725a855a45d
JoseVel4zquez <jlanconap955@icloud.com>
2022-05-27 10:44:06 -0500

    correccionfirebase

--- app/navigation/Navigation.js
+++ app/navigation/Navigation.js
@@ -1,18 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { NavigationContainer,useNavigation } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Login from '../screens/Login'
+import { View, Text } from "react-native";
+import React from "react";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AccountScreen from "../screens/Account/AccountScreen";
+import RegisterScreen from "../screens/register/RegisterScreen";
 
-const Stack = createNativeStackNavigator()
-const navigation = useNavigation
+const Stack = createNativeStackNavigator();
+const navigation = useNavigation;
 
 export default function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-          <Stack.Screen name='Iniciar sesion' navigation={navigation} component={Login} />
+        <Stack.Screen
+          name="Iniciar sesion"
+          navigation={navigation}
+          component={AccountScreen}
+        />
+        <Stack.Screen
+          name="registro"
+          navigation={navigation}
+          component={RegisterScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
--- app/screens/Login.js
+++ app/screens/Login.js
@@ -1,52 +1,69 @@
-import { View, Text,StyleSheet,ScrollView,Image } from 'react-native'
-import React from 'react'
-import { Divider } from 'react-native-elements'
-import GoogleForm from '../componets/Account/GoogleForm'
-import LoginForm from '../componets/Account/LoginForm'
+import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
+import React from "react";
+import { Button, Divider } from "react-native-elements";
+import GoogleForm from "../componets/Account/GoogleForm";
+import LoginForm from "../componets/Account/LoginForm";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Login() {
+  const navigation = useNavigation();
+  const goToRegister = () => {
+    navigation.navigate("registro");
+  };
+
   return (
     <ScrollView>
-      <Image source={require('../../assets/img/logo_black.png')} resizeMode='contain' style={styles.logo}/>
+      <Image
+        source={require("../../assets/img/logo_black.png")}
+        resizeMode="contain"
+        style={styles.logo}
+      />
       <View style={styles.viewContainer}>
-          <LoginForm />
+        <LoginForm />
+        <Text style={styles.textRegister}>
+          Registrate con tu correo institucional
+        </Text>
+        <Button
+          title="Registrarse"
+          style={styles.btnRegister}
+          onPress={goToRegister}
+        />
       </View>
       <Divider style={styles.divider} />
       <View>
-          <GoogleForm />
+        <GoogleForm />
       </View>
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    logo:{
-        width:'100%',
-        height:130
-    },
-    viewContainer:{
-        marginRight:40,
-        marginLeft:40,
-    },
-    textRegister:{
-        marginTop:15,
-        marginLeft:10,
-        marginRight:10
-    },
-    btnRegister:{
-        color:'#00a680',
-        fontWeight:'bold'
-    },
-    divider:{
-        backgroundColor:'#00a680',
-        margin:40
-    },
-    textTitle:{
-        marginTop:60,
-        textAlign: 'center',
-        fontSize:20,
-        fontWeight:'bold',
-        color:'#fff'
-    }
-
-})
+  logo: {
+    width: "100%",
+    height: 130,
+  },
+  viewContainer: {
+    marginRight: 40,
+    marginLeft: 40,
+  },
+  textRegister: {
+    marginTop: 15,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  btnRegister: {
+    color: "#00a680",
+    fontWeight: "bold",
+  },
+  divider: {
+    backgroundColor: "#00a680",
+    margin: 40,
+  },
+  textTitle: {
+    marginTop: 60,
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+});
