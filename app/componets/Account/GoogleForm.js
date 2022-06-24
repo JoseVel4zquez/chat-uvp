d2510b58a10868518091b0666358a6713f10f25c
JoseVel4zquez <jlanconap955@icloud.com>
2022-06-23 20:49:28 -0500

    interfaz

--- app/componets/Account/GoogleForm.js
+++ app/componets/Account/GoogleForm.js
@@ -5,11 +5,7 @@ import { SocialIcon } from "react-native-elements";
 export default function GoogleForm() {
   return (
     <>
-      <SocialIcon
-        title="Iniciar sesion con tu correo institucional"
-        button
-        type="google"
-      />
+      <SocialIcon title="Iniciar sesion con Google" button type="google" />
     </>
   );
 }
